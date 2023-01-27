a3c6b6ccc1a1494df7fdbb1979036609964ac2de
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-01-27 15:07:44 -0500

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const itemMarkup = createImageCards(galleryItems);
+function createImageCards(images) {
+      return images
+            .map(({ preview, original, description }) => {
+                  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`})
+              .join("");
+
+}
+
+gallery.insertAdjacentHTML("beforeend", itemMarkup);
+
+      gallery.addEventListener('click', onImageCardClick)
+
+function onImageCardClick(evt) {
+      evt.preventDefault();
+
+      if (evt.target.nodeName !== "IMG") {
+            return;
+      }
+      const galleryItem = evt.target.dataset.source;
+      const instance = basicLightbox.create(
+            `
+      <img src="${galleryItem}" width="800" height="auto">
+  `,
+      );
+      function closeModal(evt) {
+            if (evt.code === "Escape") {
+                  instance.close();
+            }
+      }
+      instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const itemMarkup = createImageCards(galleryItems);
+function createImageCards(images) {
+      return images
+            .map(({ preview, original, description }) => {
+                  return `
+      <div class="gallery__item">
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+</div>`;
+            })
+              .join("");
 
-console.log(galleryItems);
+}
+
+gallery.insertAdjacentHTML("beforeend", itemMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+captionsData: "alt",
+captionDelay: 250, });
